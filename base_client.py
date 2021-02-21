0b3a41f6a0bb3055de345d879680e4bd6705e8cb
Jonah Hendrickson <hendricksonjonah@gmail.com>
2021-02-20 19:56:10 -0600

    Updated methods

--- base_client.py
+++ base_client.py
@@ -29,85 +29,39 @@ class Client(UserClient):
             roads = self.generateRoadMap(truck)
 
         chosen_upgrade = self.select_upgrade(actions, truck)
-
+        # If there is not an active contract get one
         if(truck.active_contract is None):
-            # Get active contract
+         # Get active contract
             # Set fuel sunk costs to 0
             # Set repair sunk costs to 0
 
         else:
-            # piggyBank = money on hand - (fuel sunk costs + repair sunk costs + 2 * (fuel remaining costs + repair remaining costs))
+            # variable to track money for upgrades
+            # piggyBank = truck.money - (fuel sunk costs + repair sunk costs + 2 * ((avg * (fuel remaining)) + repair remaining costs))
+
+            #check if tires are upgraded
+            # if(TireType != "tire_econ" and piggybank >= 300):
+            #     actions.set_action(ActionType.change_tires, TireType.tire_econ)change_tires
             
-            # If(Don't have good tires and piggybank affords good tires)
-                # Buy good tires
-            # Elif(RF is not max level & Can afford to upgrade RF & Level of RF <= Level of FT)
-                # Upgrade Rabbits Foot
-            # Elif(FT is not max level & Can afford to upgrade FT)
-                # Upgrade Fuel Tank
-            # Elif(Need gas to survive next jump & Money on hand > 0)
-                # Buy Fuel
-            # Elif(Need repair to survive next jump worst case scenario & Money on hand > 0)
-                # Buy Repair
+            #upgrades rabbit's foot if the level is less than or equal to the tank level and there is piggybank money for it
+            # Elif(truck.addons.level < 3 and truck.get_cost_of_upgrade(target_addons_upgrade) < piggybank and truck.addons.level <= truck.body.level)
+                #actions.set_action(ActionType.upgrade, ObjectType.rabbitFoot)
+            
+            #upgrades fuel tank if there is piggy bank money for it
+            # Elif(truck.body.level < 3 and truck.get_cost_of_upgrade(target_body_upgrade) < piggybank)
+                #actions.set_action(ActionType.upgrade, ObjectType.tank)
+
+            # checks if gas is necessary
+            # Elif(need gas to survive and truck.money > 0)
+                # actions.set_action(ActionType.buy_gas)
+            
+            #checks if repair is necessary
+            # Elif(if truck.health <= 48 and truck.money > 0)
+                # actions.set_action(ActionType.repair)
+            
+            #drives
             # Else:
                 # Drive on optimal road
-
-
-
-
-
-        # # If there is not an active contract get one
-        # if(truck.active_contract is None):
-        #     #print("Select")
-        #     chosen_contract = self.select_new_contract(actions, truck)
-        #     actions.set_action(ActionType.select_contract, chosen_contract)
-        # # Buy gas if below 20% and there is enough money to fill tank to full at max gas price
-        # elif(truck.body.current_gas < .20 and truck.money > 100*truck.active_contract.game_map.current_node.gas_price):
-        #     #print("Gas")
-        #     actions.set_action(ActionType.buy_gas)
-        # # If health is below max and have enough money to fully repair do so
-        # elif truck.health < 100 and truck.money > 1000:
-        #     #print("Heal")
-        #     actions.set_action(ActionType.repair)
-        # elif chosen_upgrade is not None:
-        #     #print("Upgrade")
-        #     actions.set_action(ActionType.upgrade, chosen_upgrade)
-        # elif(truck.active_contract.game_map.current_node.next_node is not None):
-        #     # Move to next node
-        #     # Road can be selected by passing the index or road object
-        #     # print("Move:")
-        #     actions.set_action(ActionType.select_route, roads.pop(0))
-        
-        # pass
-
-    # These methods are not necessary, so feel free to modify or replace
-    def select_new_contract(self, actions, truck):
-        selected_contract = truck.contract_list[0]
-        for contract in truck.contract_list:
-            if contract.difficulty == ContractDifficulty.easy:
-                selected_contract = contract
-        return selected_contract
-
-    # Contract can be selected by passing the index or contract object
-    def select_upgrade(self, actions, truck):
-        target_body_upgrade = ObjectType.tank
-        target_addons_upgrade = ObjectType.rabbitFoot
-        if truck.body.level < 3 and truck.get_cost_of_upgrade(target_body_upgrade) < truck.money:
-            chosen_upgrade = target_body_upgrade
-        elif truck.addons.level < 3 and truck.get_cost_of_upgrade(target_addons_upgrade) < truck.money:
-            chosen_upgrade = target_addons_upgrade
-        else:
-            chosen_upgrade = None
-        return chosen_upgrade
-    
-    # Road can be selected by passing the index or road object
-    def select_new_route(self, actions, truck):
-        roads = truck.active_contract.game_map.current_node.roads
-        preference = 10
-        for road in roads:
-            temp = self.road_h(road)
-            if temp < preference:
-                best_road = road
-        return road
 
     # Heuristic Functions
     def road_h(self, r):
@@ -135,5 +89,3 @@ class Client(UserClient):
             temp = temp.next_node
 
         return roadMap
-
-
